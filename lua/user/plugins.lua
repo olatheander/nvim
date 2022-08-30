3a1e464484b93559267e92105f72c47731a29624
Ola Theander <ola.theander@myola.se>
2022-08-31 00:20:39 +0200

    Configured a few more plugins, mostly related to LSP and Completion

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,30 +55,44 @@ return packer.startup(function(use)
 			})
 		end,
 	})
-  use("neovim/nvim-lspconfig")
-	use("williamboman/mason.nvim")
-	use("williamboman/mason-lspconfig.nvim")
+	use("L3MON4D3/LuaSnip")
+	use("onsails/lspkind-nvim") -- vscode-like pictograms
+	use("hrsh7th/cmp-buffer") -- nvim-cmp source for buffer words
+	use("hrsh7th/cmp-nvim-lsp") -- nvim-cmp source for neovim's built-in LSP
+	use("hrsh7th/nvim-cmp") -- Completion
+	use("neovim/nvim-lspconfig") -- A collection of configurations for Neovim’s built-in LSP
+	use("williamboman/mason.nvim") -- Portable package manager for Neovim that runs everywhere Neovim runs. Easily install and manage LSP servers, DAP servers, linters, and formatters
+	use("williamboman/mason-lspconfig.nvim") -- Extension to mason.nvim that makes it easier to use lspconfig with mason.nvim
 	use("jose-elias-alvarez/null-ls.nvim") -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
-  use({
-    "SmiteshP/nvim-navic",
-    requires = "neovim/nvim-lspconfig",
-  })
 	use({
-		"nvim-treesitter/nvim-treesitter",
+		"SmiteshP/nvim-navic",
+		requires = "neovim/nvim-lspconfig",
+	})
+	use({
+		"nvim-treesitter/nvim-treesitter", -- A highly extendable fuzzy finder over lists
 	})
 	use("nvim-telescope/telescope.nvim")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use("norcalli/nvim-colorizer.lua")
 	use({
-		"nvim-lualine/lualine.nvim",
+		"nvim-lualine/lualine.nvim", -- A blazing fast and easy to configure Neovim statusline written in Lua
 		requires = { "kyazdani42/nvim-web-devicons", opt = true },
 	})
-	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" })
-  -- Git
-  use({
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      require("gitsigns").setup()
-    end,
-  })
-  use("f-person/git-blame.nvim")
-  use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
+	use({ "akinsho/bufferline.nvim", tag = "v2.*", requires = "kyazdani42/nvim-web-devicons" }) -- A snazzy bufferline
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function()
+			vim.fn["mkdp#util#install"]()
+		end,
+	})
+	-- Git
+	use({
+		"lewis6991/gitsigns.nvim", -- Git integration for buffers
+		config = function()
+			require("gitsigns").setup()
+		end,
+	})
+	use("f-person/git-blame.nvim")
+	use({ "sindrets/diffview.nvim", requires = "nvim-lua/plenary.nvim" })
 end)
