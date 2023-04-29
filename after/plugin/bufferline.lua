d0106634b09554dff2aa2584ea9c6e82df2b8ade
Ola Theander <ola.theander@myola.se>
2023-04-29 11:13:13 +0200

    Updated bufferline config

--- after/plugin/bufferline.lua
+++ after/plugin/bufferline.lua
@@ -1,12 +1,37 @@
 local bufferline = require("bufferline")
 
 bufferline.setup({
-  options = {
-    -- mode = "tabs",
-    -- separator_style = "slant",
-    always_show_bufferline = true,
-    show_buffer_close_icons = false,
-    show_close_icon = false,
-    color_icons = true,
-  },
+    options = {
+        -- mode = "tabs",
+        -- separator_style = "slant",
+        always_show_bufferline = true,
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+        color_icons = true,
+        diagnostics = "nvim_lsp",
+        separator_style = "slant",
+        indicator = {
+            style = 'underline'
+        },
+        -- highlights = {
+        --     separator = {
+        --         fg = '#073642',
+        --         bg = '#002b36',
+        --     },
+        --     separator_selected = {
+        --         fg = '#073642',
+        --     },
+        --     background = {
+        --         fg = '#657b83',
+        --         bg = '#002b36'
+        --     },
+        --     buffer_selected = {
+        --         fg = '#fdf6e3',
+        --         bold = true,
+        --     },
+        --     fill = {
+        --         bg = '#073642'
+        --     }
+        -- }
+    },
 })
