77137a02e563d4625edad892b894c6c962883596
Ola Theander <ola.theander@myola.se>
2023-11-27 11:11:51 +0100

    Added Copilot config

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -5,4 +5,16 @@ return {
     cmd = "IncRename",
     config = true,
   },
+
+  {
+    "zbirenbaum/copilot.lua",
+    opts = {
+      suggestion = { enabled = true, auto_trigger = true },
+      panel = { enabled = true, auto_refresh = true },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
 }
