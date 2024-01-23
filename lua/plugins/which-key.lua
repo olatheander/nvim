11478e0d446746db3651a402282c283c9c1d351b
Ola Theander <ola.theander@myola.se>
2024-01-23 09:50:38 +0100

    Removed unused key mapping for Git hunks

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -12,10 +12,6 @@ return {
             B = { "<cmd>Telescope file_browser<cr>", "File Browser" },
             C = { "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", "File Browser (cwd)" },
           },
-          g = {
-            j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-            k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-          },
         },
       },
     },
