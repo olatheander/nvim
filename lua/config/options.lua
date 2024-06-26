3b714f6333541f97553f4bec884e26e66ab98433
Ola Theander <ola.theander@myola.se>
2024-06-26 11:30:45 +0200

    enabled show fold sign when fold is open

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,10 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Options for the LazyVim statuscolumn
+vim.g.lazyvim_statuscolumn = {
+  folds_open = true, -- show fold sign when fold is open
+}
+
 -- Vimtex
 vim.g.vimtex_view_method = "zathura"
