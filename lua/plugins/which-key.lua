4f1ae7fe244af98c2ef1c986bdcce2bb93f01988
Ola Theander <ola.theander@myola.se>
2024-07-23 14:00:29 +0200

    Updated which-key to new format, removed unused/dupliacte mappings

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,16 +2,12 @@ return {
   {
     "folke/which-key.nvim",
     opts = {
-      defaults = {
-        ["<leader>"] = {
-          b = {
-            B = { "<cmd>lua require('telescope.builtin').buffers()<cr>", "Buffers" },
-            s = { "<Cmd>BufferLinePick<Cr>", "Select a Buffer" },
-          },
-          f = {
-            B = { "<cmd>Telescope file_browser<cr>", "File Browser" },
-            C = { "<cmd>Telescope file_browser path=%:p:h select_buffer=true<cr>", "File Browser (cwd)" },
-          },
+      defaults = {},
+      spec = {
+        {
+          mode = { "n", "v" },
+          -- { "<leader>bB", "<cmd>lua require('telescope.builtin').buffers()<cr>", desc = "Buffers" }, -- Mapped to <leader>, by default
+          { "<leader>bs", "<Cmd>BufferLinePick<Cr>", desc = "Select a Buffer" },
         },
       },
     },
