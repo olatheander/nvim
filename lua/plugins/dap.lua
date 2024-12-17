774136fcc6561fa3f39286579c18a773d6782457
Ola Theander <ola.theander@myola.se>
2024-12-17 09:22:39 +0100

    Restructured DAP config

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,5 +1,59 @@
 return {
   {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    dependencies = {
+      {
+        "williamboman/mason.nvim",
+        opts = { ensure_installed = { "java-debug-adapter", "java-test" } },
+      },
+    },
+    keys = {
+      {
+        "<F2>",
+        function()
+          require("dap").terminate()
+        end,
+        desc = "Terminate",
+      },
+      {
+        "<F5>",
+        function()
+          require("dap").continue()
+        end,
+        desc = "Start/Continue",
+      },
+      {
+        "<F7>",
+        function()
+          require("dap").step_into()
+        end,
+        desc = "Step Into",
+      },
+      {
+        "<S-F7>",
+        function()
+          require("dap").step_out()
+        end,
+        desc = "Step Out",
+      },
+      {
+        "<F8>",
+        function()
+          require("dap").step_over()
+        end,
+        desc = "Step Over",
+      },
+      {
+        "<F9>",
+        function()
+          require("dap").run_to_cursor()
+        end,
+        desc = "Run To Cursor",
+      },
+    },
+  },
+  {
     "mfussenegger/nvim-dap-python",
     ft = "python",
     dependencies = {
