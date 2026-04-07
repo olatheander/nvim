cca58c68b8c4de7c23a4f49cc74db3457087b7ff
Ola Theander <ola.theander@myola.se>
2026-04-07 20:22:43 +0200

    Add haskell-language-server and haskell-debug-adapter to MasonInstallAll

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,6 +6,7 @@ vim.api.nvim_create_user_command("MasonInstallAll", function()
       .. " typescript-language-server gopls clangd yaml-language-server"
       .. " json-lsp marksman eslint-lsp"
       .. " jdtls java-debug-adapter java-test"
+      .. " haskell-language-server haskell-debug-adapter"
       .. " stylua prettier prettierd black isort shfmt"
       .. " shellcheck markdownlint mypy ruff"
   )
