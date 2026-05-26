c4f95eb6367c4b5f136dcf14b0c8a530c9293b4c
Ola Theander <ola.theander@myola.se>
2026-05-26 14:18:03 +0200

    Enable Treesitter folding and configure LSP folding capabilities

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,74 +1,70 @@
 -- LSP server configuration (Neovim 0.12 native API)
 -- nvim-lspconfig provides the base config (cmd, filetypes, root_markers).
 -- This merges Neovim-specific settings on top.
-vim.lsp.config("lua_ls", {
-  settings = {
-    Lua = {
-      hint = { enable = true },
-      -- diagnostics = { globals = { "vim" } },
-      telemetry = { enable = false },
-    },
-  },
-})
-vim.lsp.config("pyright", {})
-vim.lsp.config("bashls", {})
-vim.lsp.config("ts_ls", {
-  settings = {
-    typescript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-        includeInlayEnumMemberValueHints = true,
-      },
-    },
-    javascript = {
-      inlayHints = {
-        includeInlayParameterNameHints = "all",
-        includeInlayFunctionParameterTypeHints = true,
-        includeInlayVariableTypeHints = true,
-        includeInlayPropertyDeclarationTypeHints = true,
-        includeInlayFunctionLikeReturnTypeHints = true,
-      },
-    },
-  },
-})
-vim.lsp.config("gopls", {
-  settings = {
-    gopls = {
-      hints = {
-        assignVariableTypes = true,
-        compositeLiteralFields = true,
-        compositeLiteralTypes = true,
-        constantValues = true,
-        functionTypeParameters = true,
-        parameterNames = true,
-        rangeVariableTypes = true,
-      },
-    },
-  },
-})
-vim.lsp.config("clangd", {})
-vim.lsp.config("yamlls", {})
-vim.lsp.config("jsonls", {})
-vim.lsp.config("marksman", {})
-vim.lsp.config("eslint", {})
-vim.lsp.config("kotlin_language_server", {})
-vim.lsp.config("sqls", {})
 
-vim.lsp.enable({
-  "lua_ls",
-  "pyright",
-  "bashls",
-  "ts_ls",
-  "gopls",
-  "clangd",
-  "yamlls",
-  "jsonls",
-  "marksman",
-  "eslint",
-  "kotlin_language_server",
-  "sqls",
-})
+local capabilities = require("blink.cmp").get_lsp_capabilities()
+
+local servers = {
+  lua_ls = {
+    settings = {
+      Lua = {
+        hint = { enable = true },
+        telemetry = { enable = false },
+      },
+    },
+  },
+  pyright = {},
+  bashls = {},
+  ts_ls = {
+    settings = {
+      typescript = {
+        inlayHints = {
+          includeInlayParameterNameHints = "all",
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+          includeInlayEnumMemberValueHints = true,
+        },
+      },
+      javascript = {
+        inlayHints = {
+          includeInlayParameterNameHints = "all",
+          includeInlayFunctionParameterTypeHints = true,
+          includeInlayVariableTypeHints = true,
+          includeInlayPropertyDeclarationTypeHints = true,
+          includeInlayFunctionLikeReturnTypeHints = true,
+        },
+      },
+    },
+  },
+  gopls = {
+    settings = {
+      gopls = {
+        hints = {
+          assignVariableTypes = true,
+          compositeLiteralFields = true,
+          compositeLiteralTypes = true,
+          constantValues = true,
+          functionTypeParameters = true,
+          parameterNames = true,
+          rangeVariableTypes = true,
+        },
+      },
+    },
+  },
+  clangd = {},
+  yamlls = {},
+  jsonls = {},
+  marksman = {},
+  eslint = {},
+  kotlin_language_server = {},
+  sqls = {},
+}
+
+for name, conf in pairs(servers) do
+  conf.capabilities = capabilities
+  vim.lsp.config(name, conf)
+end
+
+vim.lsp.enable(vim.tbl_keys(servers))
